69a6094ba5b6e4b19dd05b766fe67af5d7748f41
rvldrh <rivaldo0347t@gmail.com>
2024-09-20 15:22:48 +0700

    feat: addin persist gate state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "../redux/store"; 
+import Providers from "@/redux/Provider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider store={store}>
-          {children}
-        </Provider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Menggunakan localStorage
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer
-    },
+// Konfigurasi persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Menggabungkan semua reducer
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+
+// Menerapkan persistReducer pada rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Membuat store menggunakan persistedReducer
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Membuat persistor untuk redux-persist
+export const persistor = persistStore(store);
+
+export default store;
